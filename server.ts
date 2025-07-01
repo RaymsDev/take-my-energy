15d61338978c23dba40a117aa6e1a24e149402aa
Rayms <rayms.dev@gmail.com>
2025-07-01 15:32:31 +0200

    fix: ssr issue after migration

--- server.ts
+++ server.ts
@@ -1,8 +1,8 @@
 import { APP_BASE_HREF } from '@angular/common';
-import { CommonEngine } from '@angular/ssr';
+import { renderApplication } from '@angular/platform-server';
 import express from 'express';
-import { fileURLToPath } from 'node:url';
 import { dirname, join, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
 import bootstrap from './src/main.server';
 
 // The Express app is exported so that it can be used by serverless Functions.
@@ -11,8 +11,6 @@ export function app(): express.Express {
   const serverDistFolder = dirname(fileURLToPath(import.meta.url));
   const browserDistFolder = resolve(serverDistFolder, '../browser');
   const indexHtml = join(serverDistFolder, 'index.server.html');
-
-  const commonEngine = new CommonEngine();
 
   server.set('view engine', 'html');
   server.set('views', browserDistFolder);
@@ -29,19 +27,18 @@ export function app(): express.Express {
   );
 
   // All regular routes use the Angular engine
-  server.get('**', (req, res, next) => {
+  server.get('**', async (req, res, next) => {
     const { protocol, originalUrl, baseUrl, headers } = req;
-
-    commonEngine
-      .render({
-        bootstrap,
-        documentFilePath: indexHtml,
+    try {
+      const html = await renderApplication(bootstrap, {
+        document: indexHtml,
         url: `${protocol}://${headers.host}${originalUrl}`,
-        publicPath: browserDistFolder,
-        providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
-      })
-      .then((html) => res.send(html))
-      .catch((err) => next(err));
+        platformProviders: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
+      });
+      res.send(html);
+    } catch (err) {
+      next(err);
+    }
   });
 
   return server;
