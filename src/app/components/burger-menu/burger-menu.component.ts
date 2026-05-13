7a2e303cc85293317a5250e3c856968b1f73fa4e
sebastienDOC <s.dechand21@gmail.com>
2026-05-13 09:19:40 +0200

    style: visual redesign

--- src/app/components/burger-menu/burger-menu.component.ts
+++ src/app/components/burger-menu/burger-menu.component.ts
@@ -1,7 +1,15 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import {
+  Component,
+  HostListener,
+  Inject,
+  OnInit,
+  PLATFORM_ID,
+} from '@angular/core';
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
+import { filter } from 'rxjs/operators';
 import { SITE_CONFIG } from '../../configs';
 import { AnalyticsService } from '../../services';
-import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-burger-menu',
@@ -10,11 +18,37 @@ import { RouterModule } from '@angular/router';
   styleUrl: './burger-menu.component.scss',
   providers: [AnalyticsService],
 })
-export class BurgerMenuComponent {
+export class BurgerMenuComponent implements OnInit {
   public bookingUrl: string = SITE_CONFIG['BOOKING'];
   isMenuOpen = false;
+  isScrolled = false;
+  private isHomePage = true;
 
-  constructor(private analyticsService: AnalyticsService) {}
+  constructor(
+    private analyticsService: AnalyticsService,
+    private router: Router,
+    @Inject(PLATFORM_ID) private platformId: Object,
+  ) {}
+
+  ngOnInit(): void {
+    this.checkRoute(this.router.url);
+
+    if (this.router.events) {
+      this.router.events
+        .pipe(filter((e) => e instanceof NavigationEnd))
+        .subscribe((e: NavigationEnd) => {
+          this.checkRoute(e.urlAfterRedirects);
+          this.isMenuOpen = false;
+        });
+    }
+  }
+
+  @HostListener('window:scroll')
+  onScroll(): void {
+    if (this.isHomePage && isPlatformBrowser(this.platformId)) {
+      this.isScrolled = window.scrollY > 60;
+    }
+  }
 
   toggleMenu(): void {
     this.isMenuOpen = !this.isMenuOpen;
@@ -27,5 +61,14 @@ export class BurgerMenuComponent {
   onBookingClick(): void {
     this.analyticsService.trackEvent('booking', 'navbar');
     this.isMenuOpen = false;
+  }
+
+  private checkRoute(url: string): void {
+    this.isHomePage = url === '/' || url === '';
+    if (!this.isHomePage) {
+      this.isScrolled = true;
+    } else if (isPlatformBrowser(this.platformId)) {
+      this.isScrolled = window.scrollY > 60;
+    }
   }
 }
--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -3,6 +3,7 @@ import { HealthcareComponent } from '../../containers/healthcare/healthcare.comp
 import { AboutComponent } from './components/about/about.component';
 import { TestimonialsComponent } from './components/testimonials/testimonials.component';
 import { MainBannerComponent } from '../../components/main-banner/main-banner.component';
+import { RevealDirective } from '../../directives/reveal.directive';
 
 @Component({
   selector: 'app-home-page',
@@ -11,6 +12,7 @@ import { MainBannerComponent } from '../../components/main-banner/main-banner.co
     TestimonialsComponent,
     HealthcareComponent,
     MainBannerComponent,
+    RevealDirective,
   ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
